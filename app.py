52fba8d2c091820cb6e6c7698f0d78ba3473b4e4
KaranKurade <karankurade1@gmai.com>
2026-07-10 18:56:57 +0530

    Fix directory scanning bug

--- app.py
+++ app.py
@@ -901,4 +901,4 @@ if __name__ == "__main__":
     with app.app_context():
         db.create_all()
     logs(app)
-    app.run(host='0.0.0.0',port=5000,debug=True)
+    app.run(host='0.0.0.0',port=5000,debug=False)
